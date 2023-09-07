a21348a733f1e7c9b701f7be5cbd068a7aa5e21c
HectorSamuelMachadoAguilar <ka99059@gmail.com>
2023-09-06 21:46:50 -0600

    a

---
+++ API/Parcial2/EvitandoCors/index.js
@@ -0,0 +1,19 @@
+const express=require('express');
+const app=express();
+
+app.use(express.json());
+
+app.get("/alumnos/:carrera",(req,res)=>{
+    console.log(req.params);
+    console.log(req.query);
+    console.log(req.body);
+    res.send("Servidor Express constentando a peticion GET");
+});
+
+app.post("/alumnos",(req,res)=>{
+    res.send("Servidor Express contestando a POST");
+});
+
+app.listen(8080,()=>{
+    console.log("Servidor express escuchando");
+});
